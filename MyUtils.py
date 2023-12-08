7484f6d1fa6bc3ed43c60f49ee8c92983f86105a
imcabezas <2579904-imcabezas@users.noreply.replit.com>
2023-12-08 20:33:21 +0000

    Frontend ready for interoperability with LLM

--- MyUtils.py
+++ MyUtils.py
@@ -1,12 +1,12 @@
 
 
 # Function to format the dictionary based on inputs
-def format_dict(deliverable, subject, content, format="Markdown"):
+def format_dict(deliverable, subject, content, model, format="HTML"):
     userPrompt = f"Create a {deliverable} for high school teachers specializing in {subject}, focusing on the topic: '{content}'."
     userPrompt += " This deliverable should serve as a guide for designing engaging classroom experiences."
     outputFormat = f"Please format the response in {format}."
     data = {
-        "model": "gpt-3.5-turbo-1106",
+        "model": model,
         "messages": [
             {"role": "system", "content": "You are the most advanced AI-based NLP engine acting as an expert on the user's prompt topic and as a prompt enhancer"},
             {"role": "system", "content": "You must take user's prompt and provide an improved prompt at the end of your answer."},
--- main.py
+++ main.py
@@ -15,35 +15,18 @@ def index():
 def about():
     return "<h2>Ejemplo de mini servidor web usando Flask</h2>"
 
-@app.route('/recurso', methods = ["POST","GET"])
-def recurso():
-  if request.method == "POST":
-    return "POST Request", 201
-  if request.method == "GET":
-    return "GET Request", 200
-
-
-
-
-
-@app.route("/saludar/<string:nombre>")
-def saludar(nombre):
-	"""Retorna un mensaje que incluye el nombre establecido en la ruta """
-	return f'<h2>Hola {nombre}, bienvenido al mundo del desarrollo web!</h2>'
-
-
-
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
     # Extract form data
-    model = request.form.get('model') 
     deliverable = request.form.get('deliverable')
     subject = request.form.get('subject')
     content = request.form.get('content')
+    model = request.form.get('model') 
+    format = request.form.get('format')    
+    prompt = format_dict(deliverable, subject, content, model, format)
+    return render_template('result.html',content=prompt)
     
-    formatted_data = format_dict(deliverable, subject, content, model)
-    return f"Formatted Data: {formatted_data}"
 
 app.run(host='0.0.0.0', port=81)
